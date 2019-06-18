7b17fd93d7a290b09dc37c9b5c46da8c175e9eb4
Administrator <1159376855@qq.com>
2019-06-18 21:33:46 +0800

    绘制图像的点和线

--- Vector/VectorAdd.py
+++
@@ -1,9 +0,0 @@
-'''
-功能：两个向量的加法
-'''
-import numpy as np
-
-a = np.array([-1,2])
-b = np.array([3,1])
-
-print(a+b)
--- Vector/VectorMul.py
+++
@@ -1,9 +0,0 @@
-'''
-向量的乘法
-'''
-import numpy as np
-
-a = np.array([-1,2])
-c = 3
-
-print(c*a)
